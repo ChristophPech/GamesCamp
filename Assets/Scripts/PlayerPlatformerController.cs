0c35e87b04c19c709432f03035de674da4d1fb44
Christoph Pech <christoph.pech@gmail.com>
2018-08-07 16:57:39 +0200

    Player and Platform

---
+++ Assets/Scripts/PlayerPlatformerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlatformerController : PhysicsObject
+{
+
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    // Use this for initialization
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    public override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpTakeOffSpeed;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > 0)
+            {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        bool flipFront = (move.x > 0.01f);
+        bool flipBack = (move.x < -0.01f);
+        if(flipFront) transform.localEulerAngles = new Vector3(0,0,0);
+        if (flipBack) transform.localEulerAngles = new Vector3(0, 180, 0);
+
+
+        //animator.SetBool("grounded", grounded);
+        //animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+
+        targetVelocity = move * maxSpeed;
+        //Debug.Log(targetVelocity);
+    }
+}
+
