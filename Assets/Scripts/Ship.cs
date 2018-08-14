0c712bcf011f2b807699f887542e06c161178f83
Christoph Pech <christoph.pech@gmail.com>
2018-08-14 12:44:03 +0200

    player, rope, game scene added

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.AddForce(new Vector2(-1000f, 0));
+    }
+}
