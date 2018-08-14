19e566102233be15b9b0337b4f386b398702d4cc
Christoph Pech <christoph.pech@gmail.com>
2018-08-14 11:08:12 +0200

    MovePosition

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,17 +11,20 @@ public class Player : MonoBehaviour {
     [Range(0, 0.5f)]
     public float MouseSpeed = 0.2f;
 
+    private Rigidbody2D rb;
+
     // Use this for initialization
     void Start () {
         Debug.Log("Player - Start");
-	}
+        rb = GetComponent<Rigidbody2D>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate() {
 
         hor = Input.GetAxis("Horizontal");
         ver = Input.GetAxis("Vertical");
-        transform.position += new Vector3(hor, ver) * Time.deltaTime * KeySpeed;
+        //transform.position += new Vector3(hor, ver) * Time.deltaTime * KeySpeed;
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         Plane plane=new Plane(new Vector3(0,0,1),0);
@@ -34,9 +37,10 @@ public class Player : MonoBehaviour {
 
             Vector3 newPos = transform.position * (1.0f- MouseSpeed) + rayWorldPosition * MouseSpeed;
             Vector3 velocity=newPos - transform.position;
-            transform.position = newPos;
+            //transform.position = newPos;
+            rb.MovePosition(newPos);
 
-            GetComponent<Rigidbody2D>().velocity = velocity*(1.0f/Time.deltaTime);
+            //rb.velocity = velocity*(1.0f/Time.fixedDeltaTime);
             //Debug.Log(distance);
         }
 
