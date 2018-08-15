7e9b62c77e17cbdeefeaffe925a34902482620c7
RasElasedAustralis <mmoebis@gmx.de>
2018-08-15 10:45:46 +0200

    Waves  edited, camera direction changed

--- Assets/Scripts/WaveGenerator.cs
+++ Assets/Scripts/WaveGenerator.cs
@@ -25,6 +25,7 @@ public class WaveGenerator : MonoBehaviour {
             {
                 Enemy e=enemyObj.GetComponent<Enemy>();
                 newWave.enemies.Add(e);
+                e.gameObject.SetActive(false);
             }
 
             waves.Add(newWave);
@@ -33,6 +34,7 @@ public class WaveGenerator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        SpawnSpecificWave();
         if (Input.GetButtonDown("Fire1")) SpawnRandomWave();
 	}
 
@@ -42,6 +44,36 @@ public class WaveGenerator : MonoBehaviour {
         SpawnWave(w);
     }
 
+    // Zum waves test (wird nach wave-erstellung entfernt)
+    public void SpawnSpecificWave()
+    {
+        Wave w0 = waves[0];
+        Wave w1 = waves[1];
+        Wave w2 = waves[2];
+       // Wave w3 = waves[3];
+       // Wave w4 = waves[4];
+
+        if(Input.GetKeyDown("1")){
+            SpawnWave(w0);
+        }
+        if(Input.GetKeyDown("2"))
+        {
+            SpawnWave(w1);
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            SpawnWave(w2);
+        }
+       // if(Input.GetKeyDown("4"))
+       // {
+       //     SpawnWave(w3);
+       // }
+       // if(Input.GetKeyDown("5"))
+       // {
+       //     SpawnWave(w4);
+       // }
+    }
+
     public void SpawnWave(Wave w)
     {
         foreach(Enemy e in w.enemies)
@@ -49,6 +81,7 @@ public class WaveGenerator : MonoBehaviour {
             Enemy newEnemy=Instantiate(e.gameObject, e.transform.localPosition, e.transform.localRotation).GetComponent<Enemy>();
             newEnemy.transform.position += new Vector3(15, 0, 0);
             newEnemy.enabled = true;
+            newEnemy.gameObject.SetActive(true);
         }
     }
 }
